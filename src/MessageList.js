eecc6f66cac2804979fedca650a5619f791f6bdb
Avleen Kaur <avleen2898@gmail.com>
2018-07-03 15:01:54 -0400

    Finish basic styling

--- src/MessageList.js
+++ src/MessageList.js
@@ -4,7 +4,7 @@ import Message from './Message';
 
 const MessageList = (props) => {
     return (
-        <div className="MessageList" styles={styles.messageList}>
+        <div className="MessageList" style={styles.messageList}>
             {props.messages.map(msg => (
                 <Message message={msg} key={msg.id}/>
             ))}
