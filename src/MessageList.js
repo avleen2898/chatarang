c66d56d24fc0af48252290f85126c0f39da100db
Avleen Kaur <avleen2898@gmail.com>
2018-07-03 11:02:24 -0400

    Add messages to Chat as a state

--- src/MessageList.js
+++ src/MessageList.js
@@ -2,22 +2,10 @@ import React from 'react';
 
 import Message from './Message';
 
-const MessageList = () => {
-    const messages = [
-        {
-            id: 1,
-            userName: 'akaur',
-            body: 'DANA!!! I need help',
-        },
-        {
-            id: 2,
-            userName: 'dpalazzo',
-            body: 'I got ya! Be right there',
-        }
-    ]
+const MessageList = (props) => {
     return (
         <div className="MessageList">
-            {messages.map(msg => (
+            {props.messages.map(msg => (
                 <Message message={msg} key={msg.id}/>
             ))}
         </div>
