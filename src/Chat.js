083e7b1ccaf64094bdea9b572e08d148e87ff87e
Avleen Kaur <avleen2898@gmail.com>
2018-07-05 15:10:47 -0400

    Use syncState to send messages to firebase

--- src/Chat.js
+++ src/Chat.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import ChatHeader from './ChatHeader';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
+import base from './base';
 
 class Chat extends Component{
     constructor(){
@@ -23,6 +24,14 @@ class Chat extends Component{
         });
 
         this.setState({messages: messages});
+    }
+
+    componentDidMount(){
+        base.syncState(`messages`, {
+            context: this,
+            state: 'messages',
+            asArray: true,
+        });
     }
 
     render(){
