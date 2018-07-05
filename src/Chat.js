ab2ae721ac287c2fcbf3a20260663b813a13bcf0
Avleen Kaur <avleen2898@gmail.com>
2018-07-05 11:49:10 -0400

    Remove hard coded messages from chat

--- src/Chat.js
+++ src/Chat.js
@@ -8,20 +8,7 @@ class Chat extends Component{
     constructor(){
         super();
         this.state = {
-            messages: [
-              { id: 1, 
-                user: {
-                    uid: 123,
-                    displayName: 'Avleen',
-                    email: 'avleen2898@gmail.com'}, 
-                body: 'DANA!!! I need your help' },
-              { id: 2, 
-                user: {
-                    uid: 2737,
-                    displayName: 'Dana', 
-                    email: 'dpalazzo@fretless.com'},
-                body: 'I got ya! Be right there' },
-            ],
+            messages: [],
           }; 
     }
 
